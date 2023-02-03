a0e036a72ac87a0651245620ba923303b7a97a3c
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-03 09:29:10 +0100

    routing modifications

--- routes/items.js
+++ routes/items.js
@@ -1,5 +1,16 @@
 const items = require('../items')
 
+const Item = {
+    // type: 'object',
+    // type: 'array', 
+    // items: {
+        type: 'object',
+        properties: {
+            id: {type: 'integer'},
+            name: {type: 'string'}
+        // }
+    }
+}
 
 const getItemsOpts = {
     schema: {
@@ -7,24 +18,27 @@ const getItemsOpts = {
             200: {
                 // type: 'object',
                 type: 'array', 
-                items: {
-                    type: 'object',
-                    properties: {
-                        id: {type: 'string'},
-                        name: {type: 'string'}
-                    }
-                }
+                items: Item
             }
         }
     }
 }
+
+const getItemOpts = {
+    schema: {
+        response: {
+            200: Item
+        }
+    }
+}
+
 
 function itemRoutes(fastify, options, done) {
     fastify.get('/items',getItemsOpts, (req,reply) =>{
         reply.send( items)
     })
     
-    fastify.get('/items/:id', (req,reply) =>{
+    fastify.get('/items/:id', getItemOpts, (req,reply) =>{
         const {id} = req.params
         const item = items.find((item)=>item.id === id)
         reply.send(item)
