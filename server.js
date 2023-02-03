fde7fc5e3f581da0c187f0a7de883f66a39a3f23
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-03 08:55:24 +0100

    data add-up

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ PORT = 1111
 fastify.get('/items', (req,reply) =>{
     reply.send({test: "Hello", items: items})
 } )
+
+fastify.get('/items/:id', (req,reply) =>{
+    const {id} = req.params
+    const item = items.find((item)=>item.id === id)
+    console.log("me", id, item)
+    reply.send(item)
+} )
 const start = async () => {
     try{
        await fastify.listen(PORT)
