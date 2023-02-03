1bfb80b66ee41f65782de9f994832f337d781de0
RxGoodness <adedokuntoyyib@gmail.com>
2023-02-03 08:43:40 +0100

    data add-up

---
+++ items.js
@@ -0,0 +1,7 @@
+let items = [
+    {id : '1', name: 'First item'},
+    {id : '2', name: 'Second item'},
+    {id : '3', name: 'Third item'},
+    {id : '4', name: 'Fourth item'},
+    {id : '5', name: 'Fifth item'}
+]
